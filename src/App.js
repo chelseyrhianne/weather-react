c5749e3412a88247f489289299f901ab77da14a6
chelseyrhianne <87962835+chelseyrhianne@users.noreply.github.com>
2021-09-29 12:52:51 +1300

    Tidied code, added Bootstrap using import

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import React from "react";
+import "bootstrap/dist/css/bootstrap.css";
 import Form from "./Form.js";
 import Weather from "./Weather.js";
 import Forecast from "./Forecast.js";
 import Footer from "./Footer.js";
-
 import "./App.css";
 
 export default function App() {
