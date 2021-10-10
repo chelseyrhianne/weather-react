4f74d1a8708f97a4451a2be7061af85456b77ed2
chelseyrhianne <87962835+chelseyrhianne@users.noreply.github.com>
2021-10-10 19:45:07 +1300

    Added useEffect to Forecast for city change, removed unit conversion on current temp

--- src/WeatherTemperature.js
+++ src/WeatherTemperature.js
@@ -1,43 +1,11 @@
-import React, { useState } from "react";
+import React from "react";
 import "./WeatherTemperature.css";
 
 export default function WeatherTemperature(props) {
-  const [unit, setUnit] = useState("celsius");
-  function convertFahrenheit(event) {
-    event.preventDefault();
-    setUnit("fahrenheit");
-  }
-  function convertCelsius(event) {
-    event.preventDefault();
-    setUnit("celsius");
-  }
-
-  function fahrenheit() {
-    return (props.celsius * 9) / 5 + 32;
-  }
-  if (unit === "celsius") {
-    return (
-      <span className="WeatherTemperature current-temp">
-        {Math.round(props.celsius)}{" "}
-        <span className="unit">
-          °C |{" "}
-          <a href="/" onClick={convertFahrenheit}>
-            °F
-          </a>
-        </span>
-      </span>
-    );
-  } else {
-    return (
-      <span className="WeatherTemperature current-temp">
-        {Math.round(fahrenheit())}{" "}
-        <span className="unit">
-          <a href="/" onClick={convertCelsius}>
-            °C
-          </a>{" "}
-          | °F
-        </span>
-      </span>
-    );
-  }
+  return (
+    <span className="WeatherTemperature current-temp">
+      {Math.round(props.celsius)}
+      <span className="unit">°C</span>
+    </span>
+  );
 }
